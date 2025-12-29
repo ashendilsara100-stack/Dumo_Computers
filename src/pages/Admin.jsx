68979bf64bcd543977816c0fab0176502987578c
ashendilsara100-stack <ashendilsara100@gmail.com>
2025-12-29 10:15:28 +0530

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,32 +1,46 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { addProduct } from "../firebase/products";
 
 export default function Admin() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
 
-  const saveProduct = async () => {
-    await addProduct({ name, price: Number(price) });
-    alert("Product Added");
+  const save = async () => {
+    await addProduct({
+      name,
+      price: Number(price),
+      category,
+    });
+    alert("Product added");
   };
 
   return (
-    <div className="p-8">
+    <div className="p-8 max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-4">Admin Panel</h2>
 
-      <input placeholder="Product Name"
+      <input
+        placeholder="Product Name"
         className="border p-2 w-full mb-2"
-        onChange={e => setName(e.target.value)} />
-
-      <input placeholder="Price"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        placeholder="Category"
         className="border p-2 w-full mb-2"
-        onChange={e => setPrice(e.target.value)} />
+        onChange={(e) => setCategory(e.target.value)}
+      />
+      <input
+        placeholder="Price"
+        className="border p-2 w-full mb-2"
+        onChange={(e) => setPrice(e.target.value)}
+      />
 
-      <button onClick={saveProduct}
-        className="bg-black text-white px-4 py-2 rounded">
+      <button
+        onClick={save}
+        className="bg-black text-white px-4 py-2 rounded"
+      >
         Add Product
       </button>
     </div>
   );
 }
-
