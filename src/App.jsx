df93a695741e6f0e5df7b4dcd1bb0923ddcc1eb5
ashendilsara100-stack <ashendilsara100@gmail.com>
2025-12-29 10:11:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Builder from "./pages/Builder";
+import Checkout from "./pages/Checkout";
+import Admin from "./pages/Admin";
+
+export default function App() {
+  const [page, setPage] = useState("home");
+  const [cart, setCart] = useState([]);
+
+  return (
+    <>
+      <Navbar setPage={setPage} cartCount={cart.length} />
+      {page === "home" && <Home setPage={setPage} />}
+      {page === "shop" && <Shop cart={cart} setCart={setCart} />}
+      {page === "builder" && <Builder cart={cart} setCart={setCart} />}
+      {page === "checkout" && <Checkout cart={cart} />}
+      {page === "admin" && <Admin />}
+    </>
+  );
+}
 
