b7f2028b95eb1c29f4f17e20f228162aebddf135
ashendilsara100-stack <ashendilsara100@gmail.com>
2025-12-29 10:07:44 +0530

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,1 +1,32 @@
+import { useEffect, useState } from "react";
+import { addProduct } from "../firebase/products";
+
+export default function Admin() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const saveProduct = async () => {
+    await addProduct({ name, price: Number(price) });
+    alert("Product Added");
+  };
+
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl font-bold mb-4">Admin Panel</h2>
+
+      <input placeholder="Product Name"
+        className="border p-2 w-full mb-2"
+        onChange={e => setName(e.target.value)} />
+
+      <input placeholder="Price"
+        className="border p-2 w-full mb-2"
+        onChange={e => setPrice(e.target.value)} />
+
+      <button onClick={saveProduct}
+        className="bg-black text-white px-4 py-2 rounded">
+        Add Product
+      </button>
+    </div>
+  );
+}
 
