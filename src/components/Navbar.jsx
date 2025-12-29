c88af2423ee85428f14f06b764872069c317e70f
ashendilsara100-stack <ashendilsara100@gmail.com>
2025-12-29 07:16:11 +0000

    aa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import { ShoppingCart } from "lucide-react";
+// import { ShoppingCart } from "lucide-react";
 
-export default function Navbar({ setPage, cartCount }) {
-  return (
-    <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold cursor-pointer" onClick={() => setPage("home")}>
-        PC HUB LK
-      </h1>
+// export default function Navbar({ setPage, cartCount }) {
+//   return (
+//     <nav className="bg-black text-white px-6 py-4 flex justify-between items-center">
+//       <h1 className="text-xl font-bold cursor-pointer" onClick={() => setPage("home")}>
+//         PC HUB LK
+//       </h1>
 
-      <div className="flex gap-6 items-center">
-        <button onClick={() => setPage("shop")}>Shop</button>
-        <button onClick={() => setPage("builder")}>PC Builder</button>
-        <button onClick={() => setPage("checkout")} className="flex gap-1">
-          <ShoppingCart /> {cartCount}
-        </button>
-        <button onClick={() => setPage("admin")}>Admin</button>
-      </div>
-    </nav>
-  );
-}
+//       <div className="flex gap-6 items-center">
+//         <button onClick={() => setPage("shop")}>Shop</button>
+//         <button onClick={() => setPage("builder")}>PC Builder</button>
+//         <button onClick={() => setPage("checkout")} className="flex gap-1">
+//           <ShoppingCart /> {cartCount}
+//         </button>
+//         <button onClick={() => setPage("admin")}>Admin</button>
+//       </div>
+//     </nav>
+//   );
+// }
 
