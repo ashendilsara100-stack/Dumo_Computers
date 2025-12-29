ca88f7e018ab369d274c201f5cf70e81c24b1b70
ashendilsara100-stack <ashendilsara100@gmail.com>
2025-12-29 10:07:07 +0530

    Update products.js

--- src/firebase/products.js
+++ src/firebase/products.js
@@ -1,1 +1,11 @@
+import { db } from "./config";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+
+export const addProduct = (product) =>
+  addDoc(collection(db, "products"), product);
+
+export const getProducts = async () => {
+  const snap = await getDocs(collection(db, "products"));
+  return snap.docs.map(d => ({ id: d.id, ...d.data() }));
+};
 
