c12606bf392716d6463a0150ea07dc0a775b2262
ashendilsara100-stack <ashendilsara100@gmail.com>
2025-12-30 04:38:12 +0000

    firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBQ6HzTSnBCRJwhAkpcaog3JkxHdmK4MlU",
+  authDomain: "dumo-5adbd.firebaseapp.com",
+  projectId: "dumo-5adbd",
+  storageBucket: "dumo-5adbd.firebasestorage.app",
+  messagingSenderId: "119303130210",
+  appId: "1:119303130210:web:2511ab0bd246374e3e553b",
+  measurementId: "G-D2T5YJ16RT"
 };
 
 const app = initializeApp(firebaseConfig);
